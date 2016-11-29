9213bcb2e8d8bfa8bbaa29560f8632eabe187e51
Kalle Hüftenspeck <kalle.hilsenbek@student.hs-rm.de>
2016-11-29 14:33:48 +0100

    deleted githubpages

--- docs/master/conf.py
+++ docs/master/conf.py
@@ -32,8 +32,7 @@
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
